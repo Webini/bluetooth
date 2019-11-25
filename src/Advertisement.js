6e7c8d08ee8446ab54c01b2fca05eb8400e28614
Nicolas GASPARINI <nico@webini.fr>
2019-11-25 20:44:22 +0100

    added two new features and removed appearance flag

--- src/Advertisement.js
+++ src/Advertisement.js
@@ -15,12 +15,13 @@ class Advertisement {
     this.solicitUUIDs = [ ];
     this.serviceData = {};
     //options tx-power, appearance, local-name (https://git.kernel.org/pub/scm/bluetooth/bluez.git/tree/src/advertising.c#n413)
-    this.includes = [ 'tx-power', 'appearance' ];  
+    this.includes = [ 'tx-power' ];  // debian buster bug with appearance 
     this.localName = '';
     //https://www.bluetooth.com/ja-jp/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.gap.appearance.xml
     this.appearance = 963;
     this.duration = 0;
     this.timeout = 0;
+    this.secondaryChannel = '1M';
   }
 
   release() {
--- src/AdvertisementInterface.js
+++ src/AdvertisementInterface.js
@@ -14,6 +14,7 @@ module.exports = {
     LocalName: 's',
     Appearance: 'q',
     Duration: 'q',
-    Timeout: 'q'
+    Timeout: 'q',
+    SecondaryChannel: 's',
   }
 };
